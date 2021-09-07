f8a4ed8730cb1c7adb28406a47edd07cef96d304
giljung456 <uhwan0723@gmail.com>
2021-09-08 00:09:51 +0900

    webpack config 파일 추가

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ["@babel/polyfill", "./src/js/main.js", "./src/sass/main.scss"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src/js")],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "source-map",
+  mode: "development",
+};
